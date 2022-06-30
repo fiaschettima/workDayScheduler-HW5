2e3eb1379de84c14c733632624f9e0a3c50f4bfb
Matt F <fiaschettima@gmail.com>
2022-06-30 00:49:40 -0400

    for loop to check each time block and compare to current time hour

--- assets/script/script.js
+++ assets/script/script.js
@@ -6,5 +6,21 @@ dateHeading.text(todayIs);
 // Text description area
 var allTextarea = $('.description');
 var currentHour = moment().format('H');
-console.log(moment().hour());
-console.log(currentHour);
+var hourBlock = $('time');
+// console.log(hourBlock);
+// console.log(currentHour);
+// console.log(hourBlock[0].dateTime);
+
+for(let i = 0; i< hourBlock.length; i++){
+  
+    console.log(hourBlock[i].dateTime);
+    if(hourBlock[i].dateTime === currentHour){
+        allTextarea.addClass('present')
+    }
+    if(hourBlock[i].dateTime > currentHour){
+        allTextarea.addClass('future')
+    }
+    if(hourBlock[i].dateTime < currentHour){
+        allTextarea.addClass('past')
+    }
+}
