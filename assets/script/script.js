febfc18ffa621cc0a6243cb2dcd35a3e24ecc3a8
Matt F <fiaschettima@gmail.com>
2022-06-30 00:56:05 -0400

    add tester var to change time

--- assets/script/script.js
+++ assets/script/script.js
@@ -6,21 +6,22 @@ dateHeading.text(todayIs);
 // Text description area
 var allTextarea = $('.description');
 var currentHour = moment().format('H');
+// var currentHour = moment().set('hour' ,10);
 var hourBlock = $('time');
 // console.log(hourBlock);
 // console.log(currentHour);
 // console.log(hourBlock[0].dateTime);
-
+console.log(currentHour)
 for(let i = 0; i< hourBlock.length; i++){
   
     console.log(hourBlock[i].dateTime);
-    if(hourBlock[i].dateTime === currentHour){
-        allTextarea.addClass('present')
-    }
     if(hourBlock[i].dateTime > currentHour){
         allTextarea.addClass('future')
     }
     if(hourBlock[i].dateTime < currentHour){
         allTextarea.addClass('past')
+    }
+    if(hourBlock[i].dateTime == currentHour){
+        allTextarea.addClass('present')
     }
 }
